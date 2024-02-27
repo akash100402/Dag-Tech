bab07515eeec87d28c5aa7de4f8dc9b6671daaf9
AKASH A J <akashjayanthi2019@gmail.com>
2024-02-27 23:59:02 +0530

    create signin api routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
+
+// Auth controller for sign-up
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -31,5 +35,44 @@ export const signup = async (req, res, next) => {
   } catch (error) {
       next(error);
       
+  }
+};
+
+// Auth controller for sign-in  
+
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    //add cookies for browser
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
