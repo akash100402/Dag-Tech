419f0e1fc3f6a020f466934efc37322bcbb37be4
AKASH A J <akashjayanthi2019@gmail.com>
2024-03-04 07:42:53 +0530

    udate user profile complete

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
@@ -20,6 +21,7 @@ mongoose.connect(
 
 const app = express();
 app.use(express.json())
+app.use(cookieParser())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
