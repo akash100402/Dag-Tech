dde17575cc281768f53392a35e01644d0230aa07
AKASH A J <akashjayanthi2019@gmail.com>
2024-03-05 19:43:50 +0530

    create and post api routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 
@@ -29,6 +30,11 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes)
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+
+
+
+
 
 //error handing middleware
 app.use((err, req, res, next) => {
